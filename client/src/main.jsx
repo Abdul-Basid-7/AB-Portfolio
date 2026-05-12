296f561e439135119554d4a5671747cd0eb43398
Abdul Basid <abdulbasidabdul786786@gmail.com>
2026-05-12 15:37:23 +0530

    Fix case-sensitive skills component filename

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -38,7 +38,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <>
       <RouterProvider router={router} />
-      <Toaster position="top-right" />
+      <Toaster position="top-middle" />
     </>
   </React.StrictMode>
 );
